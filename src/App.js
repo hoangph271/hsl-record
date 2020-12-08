248e4907bbe629507c3c7a39e9ec5375dfbe7b3f
Huy Hoang Phan <hoangph271@gmail.com>
2020-12-08 23:33:32 +0700

    [💩] Workin demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useEffect, useState } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const constraints = {
+  audio: false,
+  video: true
 }
 
-export default App;
+const RECORD_STATE = {
+  UN_INITIALIZED: 'UN_INITIALIZED',
+  RECORDING: 'RECORDING',
+  RECORED: 'RECORED',
+}
+
+const RecordStatusText = ({ recordState }) => {
+  if (recordState === RECORD_STATE.UN_INITIALIZED) return null
+
+  return (
+    <div style={{
+      position: 'absolute',
+      left: 0,
+      right: 0,
+    }}>
+      {recordState}
+    </div>
+  )
+}
+
+function App() {
+  const recordChunks = useRef([])
+  const videoRef = useRef(null)
+  const streamRef = useRef(null)
+  const recordRef = useRef(null)
+  const [isReady, setIsReady] = useState(false)
+  const [recordState, setRecordState] = useState(RECORD_STATE.UN_INITIALIZED)
+
+  const handleVideoClicked = () => {
+    switch (recordState) {
+      case RECORD_STATE.RECORED:
+      case RECORD_STATE.UN_INITIALIZED: {
+        const mediaRecorder = new MediaRecorder(streamRef.current, { mimeType: 'video/webm;codecs=h264' })
+        recordRef.current = mediaRecorder
+
+        mediaRecorder.addEventListener('dataavailable', evt => recordChunks.current.push(evt.data))
+        mediaRecorder.addEventListener('stop', evt => {
+          const blob = new Blob(recordChunks.current, {
+            type: 'video/webm'
+          })
+
+          const videoUrl = window.URL.createObjectURL(blob)
+          const downloadEl = document.createElement('a')
+          downloadEl.download = 'download.mp4'
+          downloadEl.href = videoUrl
+          downloadEl.target = '_blank'
+
+          downloadEl.click()
+          console.info(downloadEl)
+        })
+        mediaRecorder.start()
+
+        setRecordState(RECORD_STATE.RECORDING)
+        break
+      }
+      case RECORD_STATE.RECORDING: {
+        recordRef.current.stop()
+        setRecordState(RECORD_STATE.RECORED)
+      }
+    }
+  }
+
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia(constraints)
+      .then(stream => {
+        streamRef.current = stream
+        setIsReady(true)
+      })
+  }, [])
+
+  useEffect(() => {
+    if (!isReady) return
+
+    videoRef.current.srcObject  = streamRef.current
+  }, [isReady])
+
+  return (
+    <div className="App">
+      {isReady ? (
+        <div>
+          <RecordStatusText recordState={recordState} />
+          <video onClick={handleVideoClicked} ref={videoRef} autoPlay style={{ width: '100%' }} />
+        </div>
+      ) : (
+        <header>{'Please allow webcam...!'}</header>
+      )}
+    </div>
+  )
+}
+
+export default App
